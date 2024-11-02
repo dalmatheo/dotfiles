cbcee568b56614dcec0f6ec595e358f0eaf7e609
dalmatheo <134540422+dalmatheo@users.noreply.github.com>
2024-11-02 21:45:12 +0100

    add submodules

--- install.sh
+++ install.sh
@@ -6,18 +6,21 @@ read answer
 
 if [ "$answer" != "${answer#[Yy]}" ] ;then 
     echo Installing dependencies
-    sudo pacman -S git base-devel hyprland hyprpaper hyprlock kitty firefox nautilus cliphist wofi adw-gtk-theme xdg-desktop-portal-gtk xdg-desktop-portal-hyprland dart-sass fd brightnessctl swww sddm zsh
+    sudo pacman -S git base-devel hyprland hyprpaper hyprlock kitty firefox nautilus cliphist wofi adw-gtk-theme xdg-desktop-portal-gtk xdg-desktop-portal-hyprland dart-sass fd brightnessctl swww sddm zsh unzip libdbusmenu-gtk3 dart-sass fd brightnessctl swww bluez bluez-utils gnome-bluetooth-3.0
     sudo systemctl enable sddm.service
     sudo mkdir /etc/sddm.conf.d/
     sudo cp ./sddm/* /etc/sddm.conf.d/
     gsettings set org.gnome.desktop.interface gtk-theme "adw-gtk3-dark"   # for GTK3 apps
     gsettings set org.gnome.desktop.interface color-scheme "prefer-dark"
     cd $HOME
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
     git clone https://aur.archlinux.org/yay.git
     cd yay
     makepkg -si
-    yay -S hyprpicker aylurs-gtk-shell bun matugen
+    cd $HOME
+    curl -fsSL https://bun.sh/install | bash
+    sudo ln -s ~/.bun/bin/bun /usr/bin/bun
+    yay -S hyprpicker aylurs-gtk-shell matugen bibata-cursor-theme
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 else
     echo "Then you'll only get the dotfiles! (and the fonts)"
 fi
