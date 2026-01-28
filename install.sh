ac6f343de0b71c5029fa8d5e9e195b3d97782379
Mathéo Dalesme-Petitalot <matheo.dalesme-petitalot@epitech.eu>
2026-01-28 19:36:57 +0100

    Improved installation script

--- install.sh
+++ install.sh
@@ -1,33 +1,88 @@
-sudo pacman -S git base-devel stow
+#!/bin/bash
 
+# 1. Install official packages
+echo "Installing base tools..."
+sudo pacman -S --needed --noconfirm git base-devel stow
+
+# 2. Setup Dotfiles
+echo "Stowing dotfiles..."
 stow .
-cd $HOME
+cd "$HOME" || exit 1
 
-printf 'Do you want to install the packages ? (Y/n)'
-read pacman
+# 3. Prompt for Official Packages (packages.txt)
+printf 'Do you want to install official packages from packages.txt? (Y/n) '
+read -r answer_official
 
-if [ "$pacman" != "${pacman#[Yy]}" ]
-then 
-    echo Installing dependencies
-    sudo pacman -S ttf-liberation ttf-roboto ttf-dejavu ttf-jetbrains-mono-nerd zsh unzip libdbusmenu-gtk3 qt6-svg cmake meson cpio pkg-config
-    hyprpm update
-    hyprpm add https://github.com/Duckonaut/split-monitor-workspaces
-    hyprpm enable split-monitor-workspaces
-    hyprpm update
-    hyprpm reload
-
-    cd $HOME
-
-    sudo systemctl enable sddm.service
-    sudo systemctl enable bluetooth.service
-    sudo mkdir /etc/sddm.conf.d/
-    sudo cp $HOME/dotfiles/sddm/configuration/* /etc/sddm.conf.d/
-    sudo cp -r $HOME/dotfiles/sddm/sddm-theme /usr/share/sddm/themes/sddm-theme
-
-    cd $HOME
-
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-else
-    echo "Then you'll only get the dotfiles!"
+if [ "$answer_official" != "${answer_official#[Yy]}" ]; then 
+    echo "Installing official packages..."
+    # Read from packages.txt, ignore comments (#) and empty lines
+    if [ -f packages.txt ]; then
+        grep -vE '^\s*#|^\s*$' packages.txt | sudo pacman -S --needed --noconfirm -
+    else
+        echo "Warning: packages.txt not found."
+    fi
+    
+    # Reload hyprpm after dependencies (if Hyprland is installed)
+    if command -v hyprpm &> /dev/null; then
+        hyprpm update
+        hyprpm add https://github.com/Duckonaut/split-monitor-workspaces
+        hyprpm enable split-monitor-workspaces
+        hyprpm update
+        hyprpm reload
+    fi
 fi
+
+# 4. Install Paru (AUR Helper)
+if ! command -v paru &> /dev/null; then
+    echo "Paru is not installed. Installing now..."
+    cd "$HOME" || exit 1
+    # Clone and install paru
+    git clone https://aur.archlinux.org/paru.git
+    cd paru || exit 1
+    makepkg -si --noconfirm
+    cd ..
+    rm -rf paru
+else
+    echo "Paru is already installed."
+fi
+
+# 5. Prompt for AUR Packages (aur.txt)
+printf 'Do you want to install AUR packages from aur.txt? (Y/n) '
+read -r answer_aur
+
+if [ "$answer_aur" != "${answer_aur#[Yy]}" ]; then
+    echo "Installing AUR packages..."
+    if [ -f aur.txt ]; then
+        # Use paru to install listed packages
+        grep -vE '^\s*#|^\s*$' aur.txt | paru -S --needed --noconfirm -
+    else
+        echo "Warning: aur.txt not found."
+    fi
+else
+    echo "Skipping AUR packages."
+fi
+
+# 6. Post-Installation Configuration
+cd "$HOME" || exit 1
+
+echo "Enabling services..."
+sudo systemctl enable sddm.service
+sudo systemctl enable bluetooth.service
+
+echo "Configuring SDDM..."
+sudo mkdir -p /etc/sddm.conf.d/
+if [ -d "$HOME/dotfiles/sddm/configuration" ]; then
+    sudo cp "$HOME"/dotfiles/sddm/configuration/* /etc/sddm.conf.d/
+fi
+
+if [ -d "$HOME/dotfiles/sddm/sddm-theme" ]; then
+    sudo cp -r "$HOME"/dotfiles/sddm/sddm-theme /usr/share/sddm/themes/sddm-theme
+fi
+
+# 7. Install Oh My Zsh (if not present)
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+    echo "Installing Oh My Zsh..."
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+fi
+
+echo "Done! You'll only get the dotfiles if you skipped the installs."
